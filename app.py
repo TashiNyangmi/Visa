c562cfedb5a82dde5d8bef7c3ba4bda3f263ac6b
Tashi Tsering Gurung <hseb.tashi@gmail.com>
2020-12-27 03:55:17 -0500

    filters worked

--- app.py
+++ app.py
@@ -2,16 +2,18 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+
+# from filter_for_app import visa_filter
+
+
+
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    # for visa_types,
-    # importing master DataFrame
     import pandas as pd
     master_df = pd.read_csv('data_cleaning/master_df.csv')
-
     countries = list(master_df['country'].unique())
     years = ['2017', '2018', '2019', '2020']
     months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september',
@@ -31,23 +33,25 @@ def index():
         returned_month = request.form['month']
         returned_visa_type = request.form['visa_type']
 
-        # Filtering data from master_df
+        from filter_for_app import visa_filter
+        result = visa_filter(returned_country, returned_year, returned_month, returned_visa_type)
 
         try:
             return render_template('index.html',
-                                   returned_country = returned_country,
-                                   returned_year = returned_year,
-                                   returned_month = returned_month,
+                                   returned_country=returned_country,
+                                   returned_year=returned_year,
+                                   returned_month=returned_month,
                                    returned_visa_type=returned_visa_type,
-                                   countries = countries,
+                                   countries=countries,
                                    years=years,
                                    months=months,
-                                   visa_types=visa_types)
+                                   visa_types=visa_types,
+                                   result=result)
         except:
             redirect('/')
-    else:                       # this is for request.method = 'GET' i.e. ~ loading the homepage for the first time
+    else:  # this is for request.method = 'GET' i.e. ~ loading the homepage for the first time
         return render_template('index.html',
-                               countries = countries,
+                               countries=countries,
                                months=months,
                                years=years,
                                visa_types=visa_types)
---
+++ filter_for_app.py
@@ -0,0 +1,22 @@
+def visa_filter(returned_country, returned_year, returned_month, returned_visa_type):
+    """filter the master_df.csv based on the given filters"""
+    import pandas as pd
+    master_df = pd.read_csv('data_cleaning/master_df.csv', index_col='date')
+    # Filtering data from master_df
+    # - creating columns year and month out of the date index
+    master_df.reset_index(inplace=True)  # convert index column:date to regular column:date
+    master_df['date'] = pd.to_datetime(master_df['date'])  # convert datatype from string to datetime
+    master_df['year'] = pd.DatetimeIndex(master_df['date']).year  # extract year number
+    master_df['month'] = pd.DatetimeIndex(master_df['date']).month  # extract month number
+    master_df['month'] = pd.to_datetime(master_df['month'],
+                                        format='%m').dt.month_name()  # convert month number to month name
+    master_df['month'] = master_df['month'].str.lower()  # lower case month names for consistency
+    master_df.set_index('date', inplace=True)  # assign column:date as index
+
+    # - actual filtering
+    result = master_df[(master_df['country'] == returned_country) &
+                       (master_df['visa_type'] == returned_visa_type) &
+                       (master_df['month'] == returned_month) &
+                       (master_df['year'] == int(returned_year))]
+    result = result['number'].values[0]
+    return result
