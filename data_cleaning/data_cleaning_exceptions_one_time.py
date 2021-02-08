d330b3ad62d35be95288180b20d4e949e6d47d82
Tashi Tsering Gurung <hseb.tashi@gmail.com>
2021-02-08 00:54:28 -0500

    DELETED LOTS OF FILES, 1)All .ipyny except data_viz, 2) All contents of pdf_raw, and csv_raw, 3)couple of .py files

--- data_cleaning/data_cleaning_exceptions_one_time.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-df = pd.read_csv('master_df.csv', index_col = 'date', parse_dates = True)
-df['visa_type'] = np.where(df['country'].isna(), df['visa_type'].str.strip("Sudan"), df['visa_type'])
-df['country'] = np.where(df['country'].isna(), "Sudan", df['country'])
-
-df.to_csv('master_df.csv')
